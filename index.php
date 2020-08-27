52d4fe98d3759d71faea9e9e426c2efc2c9be476
Razvan Migea <razvan.migea@target-e.com>
2020-08-28 00:36:37 +0300

    Fix chats bug, create responsive design and clean the code

--- index.php
+++ index.php
@@ -5,14 +5,12 @@
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
           integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
           crossorigin=""/>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-          crossorigin="anonymous">
     <link rel="stylesheet" href="main.css">
 </head>
 <body>
 
 <div id="map"></div>
+<div id="no-pin">Click on the map to start a chat!</div>
 <div class="chat-wrapper" hidden></div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
--- main.js
+++ main.js
@@ -1,123 +1,127 @@
-// Initialize the map and assign it to a variable for later use
-// var map = L.map('map', {
-//   // Set latitude and longitude of the map center (required)
-//   center: [46.7712, 23.6236],
-//   // Set the initial zoom level, values 0-18, where 0 is most zoomed-out (required)
-//   zoom: 5
-// });
-var map = L.map('map', {
+const stack = [];
+const wsUri = "ws://localhost:9000/demo/server.php";
+websocket = new WebSocket(wsUri);
+
+//Initialize the map and assign it to a variable for later use
+const map = L.map('map', {
+  //Set latitude and longitude of the map center (required)
   center: [46.7712, 23.6236],
-  zoom: 4,
+  //Set the initial zoom level, values 0-18, where 0 is most zoomed-out (required)
+  zoom: 5
 });
 
-var tiles = new L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png', {
+// Create a Tile Layer and add it to the map
+const tiles = new L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png', {
   attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
 }).addTo(map);
-var stack = [];
 
 function onMapClick(e) {
-  var pin = L.marker(e.latlng).addTo(map);
-  pin.bindPopup(showChat(pin)).openPopup();
-
+  const pin = L.marker(e.latlng).addTo(map);
+  pin.bindPopup('Chatroom ' + pin._leaflet_id).openPopup();
+  console.log('on map click start server');
+  startServer(pin);
 }
 
-function showChat(pin) {
-  return 'Chat ' + pin._leaflet_id;
-}
-
-map.on('click', onMapClick);
-map.on('popupopen', function(e) {
-  $('.chat-wrapper').removeAttr('hidden');
-  var pin = e.popup._source;
-  var pinID = pin._leaflet_id;
-
+function changeChat(pinID) {
   $('#message-box-' + pinID).show();
   $('#user-panel-' + pinID).show();
   previousPinID = stack.pop();
-  stack.push(pin._leaflet_id);
+  stack.push(pinID);
   $('#message-box-' + previousPinID).hide();
   $('#user-panel-' + previousPinID).hide();
+}
 
-    if($('#message-box-' + pin._leaflet_id).length == 0){
-    $('.chat-wrapper').append('<div class="message-box" id="message-box-' + pin._leaflet_id + '" ></div>\n' +
-        '    <div class="user-panel" id="user-panel-' + pin._leaflet_id + '">\n' +
-        '        <input type="text" name="name" class="name" id="name-' + pin._leaflet_id + '" placeholder="Your Name" maxlength="15" />\n' +
-        '        <input type="text" name="message" class="message" id="message-' + pin._leaflet_id + '" placeholder="Type your message here..." maxlength="100" />\n' +
-        '        <button class="send-message" id="send-message-' + pin._leaflet_id + '">Send</button>\n' +
-        '    </div>');
-  }
-  startServer(pin);
-});
-
-//create a new WebSocket object.
+//Create a new WebSocket object.
 function startServer(pin) {
-  var pinID = pin._leaflet_id;
-  var msgBox = $('#message-box-' + pinID);
-  var wsUri = "ws://localhost:9000/demo/server.php";
-  websocket = new WebSocket(wsUri);
 
-  websocket.onopen = function(ev) { // connection is open
-    msgBox.append('<div class="system_msg" style="color:#bbbbbb">Welcome to chat no. ' + pinID + '!</div>'); //notify user
-  };
-// Message received from server
+  const pinID = pin._leaflet_id;
+
+  let msgBox = $('#message-box-' + pinID);
+  msgBox.append('<div class="system_msg" style="color:#bbbbbb; text-align: center">Chatroom ' + pinID + '!</div>'); //Notify user
+
+//Message received from server
   websocket.onmessage = function(ev) {
-    var response 		= JSON.parse(ev.data); //PHP sends Json data
+    let response 		= JSON.parse(ev.data); //PHP sends Json data
 
-    var res_type 		= response.type; //message type
-    var user_message 	= response.message; //message text
-    var user_name 		= response.name; //user name
-    var user_color 		= response.color; //color
+    let res_type 		= response.type; //Message type
+    let user_message 	= response.message; //Message text
+    let user_name 		= response.name; //User name
+    let chat_id         = response.chat_id;
+    msgBox = $('#message-box-' + chat_id);
 
     switch(res_type){
       case 'usermsg':
-        msgBox.append('<div><span class="user_name" style="color:' + user_color + '">' + user_name + '</span> : <span class="user_message">' + user_message + '</span></div>');
+        msgBox.append('<div><span class="user-name" id="user-name-' + chat_id + '">' + user_name + '</span> : <span class="user-message" id="user-message- ' + chat_id + ' ">' + user_message + '</span></div>');
         break;
       case 'system':
         msgBox.append('<div style="color:#bbbbbb">' + user_message + '</div>');
         break;
     }
-    msgBox[0].scrollTop = msgBox[0].scrollHeight; //scroll message
-
+    msgBox[0].scrollTop = msgBox[0].scrollHeight; //Scroll message
   };
 
   websocket.onerror	= function(ev){ msgBox.append('<div class="system_error">Error Occurred - ' + ev.data + '</div>'); };
-  websocket.onclose 	= function(ev){ msgBox.append('<div class="system_msg">Connection Closed</div>'); };
+  websocket.onclose = function(){ msgBox.append('<div class="system_msg">Connection Closed</div>'); };
 
 //Message send button
   $('#send-message-' + pinID).click(function(){
-    send_message(pinID);
+    sendMessage(pinID);
   });
 
 //User hits enter key
-  $('#message-' + pin._leaflet_id).on( "keydown", function( event ) {
+  $('#message-' + pinID).on( "keydown", function( event ) {
     if(event.which==13){
-      send_message(pin._leaflet_id);
+      sendMessage(pinID);
     }
   });
 
 //Send message
-  function send_message(pinID){
+  function sendMessage(pinID){
     console.log(pinID);
-    var message_input = $('#message-' + pinID); //user message text
-    var name_input = $('#name-' + pinID); //user name
+    const message_input = $('#message-' + pinID); //User message text
+    const name_input = $('#name-' + pinID); //User name
 
-    if(name_input.val() == ""){ //empty name?
-      // alert("Enter your Name please!");
+    if(name_input.val() == ""){ //Empty name?
+      alert("Enter your Name please!");
       return;
     }
-    if(message_input.val() == ""){ //empty message?
-      // alert("Enter Some message Please!");
+    if(message_input.val() == ""){ //Empty message?
+      alert("Enter Some message Please!");
       return;
     }
 
-    //prepare json data
-    var msg = {
+    //Prepare json data
+    const msg = {
       message: message_input.val(),
-      name: name_input.val()
+      name: name_input.val(),
+      chat_id: pinID
     };
+
     //convert and send data to server
     websocket.send(JSON.stringify(msg));
-    message_input.val(''); //reset message input
+    message_input.val(''); //Reset message input
+
   }
 }
+
+//Listeners
+map.on('click', onMapClick);
+map.on('popupopen', function(e) {
+  $('#no-pin').hide(); //Hide first message
+  $('.chat-wrapper').removeAttr('hidden'); //Show chat container
+  const pin = e.popup._source;
+  const pinID = pin._leaflet_id;
+  changeChat(pinID);
+
+//Create new chat if not exist
+  if($('#message-box-' + pinID).length == 0){
+    $('.chat-wrapper').append('<div class="message-box" id="message-box-' + pin._leaflet_id + '" ></div>\n' +
+      '    <div class="user-panel" id="user-panel-' + pin._leaflet_id + '">\n' +
+      '        <input type="text" name="name" class="name" id="name-' + pin._leaflet_id + '" placeholder="Your Name" maxlength="15" />\n' +
+      '        <input type="text" name="message" class="message" id="message-' + pin._leaflet_id + '" placeholder="Type your message here..." maxlength="100" />\n' +
+      '        <button class="send-message" id="send-message-' + pin._leaflet_id + '">Send</button>\n' +
+      '    </div>');
+  }
+});
+
 
